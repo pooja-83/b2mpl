f3199361a11e8906a88c87e3e3a39a7b6037ae2e
K Mahender <K Mahender@DESKTOP-N7B7J90>
2021-06-08 15:27:46 +0530

    First time uploaded project structure

---
+++ cohort/src/main/java/cohort/Ebay.java
@@ -0,0 +1,34 @@
+package cohort;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Ebay {
+
+	public static void main(String[] args) throws Exception
+	{
+		
+		ChromeDriver driver=new ChromeDriver();	//open firefox browser
+		driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
+		driver.get("http://www.ebay.com");  		//open the given url
+		driver.findElement(By.id("gh-ac")).sendKeys("Laptops");	
+		driver.findElement(By.id("gh-btn")).click();
+		Thread.sleep(5000);
+		List<WebElement> lst=driver.findElements(By.xpath("//h3[contains(text(),'Lenovo')]"));
+		String str;
+		for(int i=0;i<lst.size();i++)
+		{
+			str=lst.get(i).getText();
+			System.out.println("name:"+str);
+		 
+
+	    }
+		Thread.sleep(5000);
+		driver.close();
+
+}
+}
